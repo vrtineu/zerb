18daee15233aeb306b285504f301bbf24e1a0566
vrtineu <vininro@gmail.com>
2023-08-10 00:17:11 -0300

    feat: creating method to find the closest partner

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,4 +3,10 @@
 class Partner < ActiveRecord::Base
   validates :trading_name, :owner_name, :document, :coverage_area, :address, presence: true
   validates :document, uniqueness: true
+
+  def self.find_nearest(lat, long)
+    point = RGeo::Geographic.spherical_factory(srid: 4326).point(long, lat)
+    partner = Partner.arel_table
+    Partner.order(partner[:coverage_area].st_distance(point)).order(partner[:address].st_distance(point)).first
+  end
 end
